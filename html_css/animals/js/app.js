d2de95a8ac1dadbab0ab128c231ffb859e9b4f8a
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-01-28 08:29:35 +0300

    Update app.js

--- html_css/animals/js/app.js
+++ html_css/animals/js/app.js
@@ -22,12 +22,21 @@ document.querySelector('.burger-icon-click-close').addEventListener('click', () 
 });
 
 /*--окно волонтерство */
-document.querySelector('.volunteering-open').addEventListener('click', () => {
-    document.querySelector('.modal-windows__volunteering').classList.toggle('modal-window-active');
-    document.body.style.overflow = 'hidden';
-});
+// document.querySelector('.volunteering-open').addEventListener('click', () => {
+//     document.querySelector('.modal-windows__volunteering').classList.toggle('modal-window-active');
+//     document.body.style.overflow = 'hidden';
+// });
 
 document.querySelector('.volunteering-close-window').addEventListener('click', () => {
     document.querySelector('.modal-windows__volunteering').classList.toggle('modal-window-active');
     document.body.style.overflow = '';
 });
+
+function showModal (classClick, classWindow, classToggle) {
+    document.querySelector(classClick).addEventListener('click', () => {
+        document.querySelector(classWindow).classList.toggle(classToggle);
+        document.body.style.overflow = 'hidden';
+    });
+}
+
+showModal('.volunteering-open', '.modal-windows__volunteering', 'modal-window-active');
