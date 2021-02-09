c3993ccdada15cd3be876d2bb7259be0a6824e04
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-02-09 18:52:02 +0300

    add dev-server

--- html_css/webpack.config.js
+++ html_css/webpack.config.js
@@ -14,36 +14,44 @@ const isProd = !isDev;
 const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: './js/main.js',
-    output: {
-      filename: `./js/${filename('js')}`,
-      path: path.resolve(__dirname, 'app'),
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html'),
-            filename: 'index.html',
-            minify: {
-                collapseWhitespace: isProd
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: `./css/${filename('css')}`
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-        ]
-    }
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './js/main.js',
+  output: {
+    filename: `./js/${filename('js')}`,
+    path: path.resolve(__dirname, 'app'),
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'app'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename('css')}`
+    }),
+  ],
+  module: {
+    rules: [
+      {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ]
+  }
 };
