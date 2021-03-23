cea4bd582aae1d39503d7092fdb4003ddeccc2bf
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-23 15:46:43 +0300

    feat: objects complite

---
+++ js-course/objects.js
@@ -0,0 +1,16 @@
+// Объекты
+// Создать объект с полем 'notebook' равным 'MacBook'
+let laptop = {
+  notebook: 'Macbook'
+}
+console.log(laptop.notebook)
+// Добавить в объект из предыдущей задачи поле 'price', равное 1500 и поле currensy, равная 'dollar'
+laptop.price = 1500;
+laptop.currensy = 'dollar';
+console.log(laptop)
+// Добавить поле details, которое будет содержать объект с полями model и color (значения этих полей задайте сами) 
+laptop.details = {
+  model: 'Air',
+  color: 'Silver'
+}
+console.log(laptop)
