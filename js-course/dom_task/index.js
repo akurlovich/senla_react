7510e65053a3a1fdbf78d3a27f81f4f902199a35
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-30 15:21:41 +0300

    feat: second DOM task

--- js-course/dom_task/index.js
+++ js-course/dom_task/index.js
@@ -6,24 +6,43 @@ document.addEventListener('DOMContentLoaded', function () {
 // Создать функцию которая принимает два. Функция проверяет является ли первый элемент родителям для второго элемента isParent(parent, child) => true || false
 
   const isParent = (parent, child) => {
+    let bool = false;
     for (let i = 0; i < parent.childNodes.length; i++) {
-      console.log(parent.childNodes[i]);
-      console.log(child);
-      (parent.childNodes[i] == child) ? console.log('True') : console.log('False');
+      if (parent.children[i] == child) {
+        bool = true;
+      };
     }
+    console.log(bool);
   }
   
   const first = document.querySelector('ul');
-  // console.log(first);
-  // console.log(first.childNodes);
   const second = document.querySelector('li');
-  // console.log(second);
-  isParent(first, second);
+  
+  // isParent(first, second);
 
 // Найти элемент который находится перед списком ul
+
+  // console.log(first.previousElementSibling);
+
+
 // Найти параграф и получить его текстовые содержимое
+  const paragraph = document.querySelector('p');
+  // console.log(paragraph.textContent);
+
 // Создать функцию, которая принимает в качестве аргумента узел DOM и возвращает информацию виде объекта о типе узла, имени узла и количестве дочерних узлов.
+
+const objInfo = element => {
+  let objectInfo = {};
+  objectInfo.type = element.nodeType;
+  objectInfo.name = element.nodeName;
+  objectInfo.childrens = element.children.length;
+  console.log(objectInfo);
+}
+
+objInfo(first);
+
 // Найти список и добавить ему класс "list"
+first.classList.add('list');
 
 
   
