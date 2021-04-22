d921ce31b4091713d449fbfdf1efd91f65c628ff
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-04-22 20:15:59 +0300

    feat: nav btns

--- todo_app/index.js
+++ todo_app/index.js
@@ -3,7 +3,8 @@
 let addMessage = document.querySelector('.input-task'),
     addButton = document.querySelector('.task-btn'),
     delButtons = document.querySelectorAll('.del-btn'),
-    todo = document.querySelector('.todo-list');
+    todo = document.querySelector('.todo-list'),
+    nav = document.querySelector('.navigation');
 
 let todoList = [];
 
@@ -33,8 +34,8 @@ function displayMessages() {
     <div class="item" id='item_${i}'>
       <img class="star ${item.important ? 'star__active' : ''}" id='star-item_${i}' src="./img/star.svg" alt="">
       <p class="task-text ${item.checked ? 'task-checked' : ''}" id='task-item_${i}'>${item.todo}</p>
-      <button class="mark-btn ${item.important ? 'btn__none' : ''}" id='mark-item_${i}'>MARK IMPORTANT</button>
-      <button class="not-mark-btn ${item.important ? '' : 'btn__none'}" id='not-mark-item_${i}'>not IMPORTANT</button>
+      <button class="mark-btn ${item.important ? 'btn__none' : ''}" id='mark-item_${i}' style="display: ${item.checked ? 'none' : ''}">MARK IMPORTANT</button>
+      <button class="not-mark-btn ${item.important ? '' : 'btn__none'}" id='not-mark-item_${i}' style="display: ${item.checked ? 'none' : ''}">not IMPORTANT</button>
       <img class="del-btn" id='btn-item_${i}' src="./img/Delete.svg" alt="">
     </div>
     `; 
@@ -43,16 +44,47 @@ function displayMessages() {
   localStorage.setItem('todo', JSON.stringify(todoList));
 };
 
-// delButtons.forEach((item, index) => {
-//   item.addEventListener('click', function(event) {
-//     console.log(event.target);
-//     console.log(index);
-//   });
-// });
+function displayActive() {
+  let displayMessage = '';
+  todoList.forEach((item, i) => {
+    if (item.important) {
+      displayMessage += `
+      <div class="item" id='item_${i}'>
+        <img class="star ${item.important ? 'star__active' : ''}" id='star-item_${i}' src="./img/star.svg" alt="">
+        <p class="task-text ${item.checked ? 'task-checked' : ''}" id='task-item_${i}'>${item.todo}</p>
+        <button class="mark-btn ${item.important ? 'btn__none' : ''}" id='mark-item_${i}'>MARK IMPORTANT</button>
+        <button class="not-mark-btn ${item.important ? '' : 'btn__none'}" id='not-mark-item_${i}'>not IMPORTANT</button>
+        <img class="del-btn" id='btn-item_${i}' src="./img/Delete.svg" alt="">
+      </div>
+      `; 
+      todo.innerHTML = displayMessage;
+    }
+  })
+  localStorage.setItem('todo', JSON.stringify(todoList));
+};
+
+function displayDone() {
+  let displayMessage = '';
+  todoList.forEach((item, i) => {
+    if (item.checked) {
+      displayMessage += `
+      <div class="item" id='item_${i}'>
+        <img class="star ${item.important ? 'star__active' : ''}" id='star-item_${i}' src="./img/star.svg" alt="">
+        <p class="task-text ${item.checked ? 'task-checked' : ''}" id='task-item_${i}'>${item.todo}</p>
+        <button class="mark-btn ${item.important ? 'btn__none' : ''}" id='mark-item_${i}'>MARK IMPORTANT</button>
+        <button class="not-mark-btn ${item.important ? '' : 'btn__none'}" id='not-mark-item_${i}'>not IMPORTANT</button>
+        <img class="del-btn" id='btn-item_${i}' src="./img/Delete.svg" alt="">
+      </div>
+      `; 
+      todo.innerHTML = displayMessage;
+    }
+  })
+  localStorage.setItem('todo', JSON.stringify(todoList));
+};
 
 todo.addEventListener('click', (event) => {
   let targetIndex = event.target.getAttribute('id').slice(-1);
-  console.log(event.target.classList);
+  // console.log(event.target.classList);
 
   if (event.target.classList.contains('del-btn')) {
     todoList.splice(targetIndex, 1);
@@ -92,3 +124,30 @@ todo.addEventListener('click', (event) => {
   displayMessages();
 
 });
+
+nav.addEventListener('click', function(event) {
+  if (event.target.classList.contains('li-all')) {
+    document.querySelector('.li-all').classList.add('active');
+    document.querySelector('.li-about').classList.remove('active');
+    document.querySelector('.li-done').classList.remove('active');
+    document.querySelector('.add-tasks').classList.remove('add-none');
+    displayMessages();
+
+  }
+  if (event.target.classList.contains('li-about')) {
+    document.querySelector('.li-about').classList.add('active');
+    document.querySelector('.li-all').classList.remove('active');
+    document.querySelector('.li-done').classList.remove('active');
+    document.querySelector('.add-tasks').classList.remove('add-none');
+    displayActive();
+
+  }
+  if (event.target.classList.contains('li-done')) {
+    document.querySelector('.li-done').classList.add('active');
+    document.querySelector('.li-all').classList.remove('active');
+    document.querySelector('.li-about').classList.remove('active');
+    document.querySelector('.add-tasks').classList.add('add-none');
+    displayDone();
+
+  }
+})
