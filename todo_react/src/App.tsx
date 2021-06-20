264644ede36cdf7e56af149c8405b8726fddf923
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-06-20 17:18:33 +0300

    feat img

--- todo_react/src/App.tsx
+++ todo_react/src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import Card, { CardVariant } from './components/Card';
-import UserList from './components/UserList';
-import { IUser } from './types/types';
+import './css/style.css';
+import mainBgImg from './img/taking_notes.png'
 
 class App extends React.Component {
   render() {
-
     return (
-      <div>
-        <Card variant={CardVariant.outlined} width='200px' height='100px'>
-          <button>Button click</button>
-        </Card>
-        <UserList users={users} />
+      <div className='wrapper'>
+        <div className="container">
+          <img src={mainBgImg} />
+        </div>
       </div>
     );
   }
